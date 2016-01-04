e06e0fbd3b8ee66155687795bca3847534148d35
Israel Santana <isantana@inerza.com>
2016-01-04 17:15:31 +0000

    Se crea un método para comprobar las cabeceras de ficheros gestionados por ansible

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -9,4 +9,10 @@ RSpec.configure do |c|
   else
     c.sudo_password = ENV['SUDO_PASSWORD']
   end
+end
+
+def check_ansible_header (f)
+  describe file f do
+   a = it { should contain "# This file is autogenerate by ansible\n# Do not edit, changes will be overwritten" }
+  end
 end  
